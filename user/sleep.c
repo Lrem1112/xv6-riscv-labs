4827918cc9fd8eff6369e9e3607f3837c095e1dc
Lrem <2657274798@qq.com>
2026-07-13 19:46:12 +0800

    init: 完成调试环境配置和sleep()功能

---
+++ user/sleep.c
@@ -0,0 +1,15 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char **argv) {
+  int n;
+
+  if (argc != 2) {
+    fprintf(2, "Usage: sleep [ticks]\n");
+    exit(1);
+  }
+  
+  n = atoi(argv[1]);
+  pause(n);
+  exit(0);
+}
