eca9c854897d6aaebaebe9d8f9ee193fc0a88a2f
Lrem <2657274798@qq.com>
2026-07-20 00:31:40 +0800

    sol: 完成find-exec，结束util lab

--- user/find.c
+++ user/find.c
@@ -1,6 +1,8 @@
 #include "kernel/types.h"
+
 #include "kernel/fcntl.h"
 #include "kernel/fs.h"
+#include "kernel/param.h"
 #include "kernel/stat.h"
 #include "user/user.h"
 
@@ -12,7 +14,23 @@ int fnamecmp(char *path, char *f) {
   return strcmp(p, f);
 }
 
-void find(char *path, char *f) {
+void run_exec(char *file, int cmdc, char **cmdv) {
+  char *eargv[MAXARG];
+  for (int i = 0; i < cmdc; i++)
+    eargv[i] = cmdv[i];
+  eargv[cmdc] = file;
+  eargv[cmdc + 1] = 0;
+
+  int pid = fork();
+  if (pid == 0) {
+    exec(eargv[0], eargv);
+    fprintf(2, "find: exec %s failed\n", eargv[0]);
+    exit(1);
+  }
+  wait(0);
+}
+
+void find(char *path, char *f, int exec, int cmdc, char **cmdv) {
   int fd;
   char buf[512], *p;
   struct dirent de;
@@ -33,8 +51,12 @@ void find(char *path, char *f) {
   case T_DEVICE:
     break;
   case T_FILE:
-    if (!fnamecmp(path, f))
-      printf("%s\n", path);
+    if (!fnamecmp(path, f)) {
+      if (exec)
+        run_exec(path, cmdc, cmdv);
+      else
+        printf("%s\n", path);
+    }
     break;
   case T_DIR:
     strcpy(buf, path);
@@ -47,7 +69,7 @@ void find(char *path, char *f) {
         continue;
       memmove(p, de.name, sizeof(de.name));
       p[sizeof(de.name)] = 0;
-      find(buf, f);
+      find(buf, f, exec, cmdc, cmdv);
     }
     break;
   }
@@ -55,11 +77,14 @@ void find(char *path, char *f) {
 }
 
 int main(int argc, char *argv[]) {
-  if (argc != 3) {
+  if (argc < 3) {
     printf("Usage: find <dir> <filename>\n");
     exit(0);
-  }
-
-  find(argv[1], argv[2]);
+  } else if (argc == 3)
+    find(argv[1], argv[2], 0, argc, argv);
+  else if (argc > 4 && strcmp(argv[3], "-exec") == 0)
+    find(argv[1], argv[2], 1, argc - 4, argv + 4);
+  else
+    printf("Usage: find <dir> <filename> -exec <cmd>\n");
   exit(0);
 }
