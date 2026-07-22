a43362b585406d10e145887031c136c17d672c0f
Lrem <2657274798@qq.com>
2026-07-22 22:15:36 +0800

    sol: 完成attack，结束syscall lab

--- user/attack.c
+++ user/attack.c
@@ -3,10 +3,21 @@
 #include "user/user.h"
 #include "kernel/riscv.h"
 
+#define DATASIZE (9*4096)
+
 int
 main(int argc, char *argv[])
 {
   // Your code here.
-
+  char *buf = sbrk(DATASIZE);
+  int n = 0;
+  for (; n < DATASIZE; ++n, ++buf) {
+    if (*buf == 'T') {
+      if (strcmp("This may help.", buf) == 0) {
+        printf("%s\n", buf + 16);
+        exit(0);
+      }
+    }
+  }
   exit(1);
 }
